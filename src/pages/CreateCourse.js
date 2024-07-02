12b62392e8d3eb694fda0a6c93b3b3d271660c4f
Xuanphuc <120350759+Phuc1206@users.noreply.github.com>
2024-07-02 10:36:16 +0700

    config css

--- src/pages/CreateCourse.js
+++ src/pages/CreateCourse.js
@@ -1,15 +1,41 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 function CreateCourse(){
+    const initialValues = {}
+    const onSubmit = (data) =>{
+        console.log(data);
+    }
     return(
-        <div className="CreateCourse">
-            <h2>Create a new Course</h2>
-            <form>
-                <input type="text" placeholder="Course Title"/>
-                <input type="text" placeholder="Course Description"/>
-                <input type="text" placeholder="Course Video ID"/>
-                <button type="submit">Submit</button>
-            </form>
-            
+        <div className="CreateCoursePage">
+            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+                <Form>
+                    <label>Course name</label>
+                    <Field 
+                        autocomplete="off"
+                        name="name"
+                        placeholder="ex. HTML"
+                        />
+                    <label>Course description</label>
+                    <Field
+                        autocomplete="off"
+                        name="description"
+                        placeholder="desc"
+                        />
+                    <label>Course video id</label>
+                    <Field
+                        autocomplete="off"
+                        name="videoId"
+                        placeholder="video id"
+                        />
+                    <label>Course level</label>
+                    <Field
+                        autocomplete="off"
+                        name="level"
+                        placeholder="number of levels"
+                        />
+                </Form>
+            </Formik>
         </div>
     )
 }

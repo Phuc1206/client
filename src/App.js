1434d50f391f5e5dcc59bf2cff5c595102e7ff0e
Xuanphuc <120350759+Phuc1206@users.noreply.github.com>
2024-07-03 23:45:28 +0700

    config prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import CreateCourse from './pages/CreateCourse'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateCourse from './pages/CreateCourse';
+import Login from './pages/Login';
 function App() {
-  
-  return (
-    <div className="App">
-      <Router>
-        <Link to="/createcourse/">Create Course</Link>
-        <Link to="/">Home page</Link>
-        <Routes>
-          <Route path='/' exact element={<Home />}/>
-          <Route path='/createcourse/' element={<CreateCourse />}/>
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Link to="/createcourse/">Create Course</Link>
+                <Link to="/">Home page</Link>
+                <Link to="/login">Login</Link>
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/createcourse/" element={<CreateCourse />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- src/pages/CreateCourse.js
+++ src/pages/CreateCourse.js
@@ -2,38 +2,54 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 function CreateCourse(){
-    const initialValues = {}
+    const initialValues = {
+        name: "",
+        description: "",
+        videoId: "",
+        level: "",
+    }
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().required("Name is required"),
+        description: Yup.string().required("Description is required"),
+        videoId: Yup.string().required("Video id is required"),
+        level: Yup.number().min(0).max(5).required("Level is required"),
+    })
     const onSubmit = (data) =>{
         console.log(data);
     }
     return(
         <div className="CreateCoursePage">
-            <Formik initialValues={initialValues} onSubmit={onSubmit}>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
                 <Form>
                     <label>Course name</label>
+                    <ErrorMessage name="name" component="div" className="error" />
                     <Field 
-                        autocomplete="off"
+                        autoComplete="off"
                         name="name"
                         placeholder="ex. HTML"
                         />
                     <label>Course description</label>
+                    <ErrorMessage name="name" component="div" className="error" />
                     <Field
-                        autocomplete="off"
+                        autoComplete="off"
                         name="description"
                         placeholder="desc"
                         />
                     <label>Course video id</label>
+                    <ErrorMessage name="name" component="div" className="error" />
                     <Field
-                        autocomplete="off"
+                        autoComplete="off"
                         name="videoId"
                         placeholder="video id"
                         />
                     <label>Course level</label>
+                    <ErrorMessage name="name" component="div" className="error" />
                     <Field
-                        autocomplete="off"
+                        autoComplete="off"
                         name="level"
                         placeholder="number of levels"
                         />
+                        <button type="submit">Submit</button>
                 </Form>
             </Formik>
         </div>
